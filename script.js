099621ea6abb88f8bb819d0461455fc363b5940f
blakelyupton <blakelyupton@gmail.com>
2026-04-16 00:54:39 +0800

    Fix bug with scoring system to update each round

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getRandomNumber() {
     return Math.floor(Math.random() * 3) + 1;
 }
 
 function getComputerChoice() {
     let number = getRandomNumber(); 
-    
+
     if (number === 1) {
     return "rock";
     } else if (number === 2) {
@@ -21,13 +24,28 @@ function playRound(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) {
         console.log ("DRAW, FUCKO!"); 
         }    else if (playerChoice === "rock" && computerChoice === "scissors") {
+        playerScore++;    
         console.log ("YOU ARE ALL THAT IS MAN!"); 
         }    else if (playerChoice === "paper" && computerChoice === "rock") {
-        console.log ("YOUR PENIS IS BIG-TIME!"); 
+            playerScore++;
+            console.log ("YOUR PENIS IS BIG-TIME!"); 
         }   else if (playerChoice === "scissors" && computerChoice === "paper") {
-        console.log ("DOG POLICE!"); 
+            playerScore++;
+            console.log ("DOG POLICE!"); 
         }
-    else {
+             else if (computerChoice === "rock" && playerChoice === "scissors") {
+        computerScore++;
         console.log ("YOUR ASS GOT SACKED!!!");
     }
+        else if (computerChoice === "scissors" && playerChoice === "paper") {
+        computerScore++;
+        console.log ("YOUR ASS GOT SACKED!!!");
+        }
+        else if (computerChoice === "paper" && playerChoice === "rock") {
+        computerScore++;
+        console.log ("YOUR ASS GOT SACKED!!!");
+
+}
+         console.log("Snapping Turtle Score", playerScore);
+         console.log("JD Vance score", computerScore);
 }
